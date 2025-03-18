4a80d3122a0bc11a3172865ae99bbb9934a2ee4a
Ramesh Krishnan <ramesh.krishnan.de@gmail.com>
2025-03-18 16:09:16 +0100

    Http fetch and loading message

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import NewPost from "./NewPost.jsx";
 import Post from "./Post.jsx";
@@ -6,10 +6,34 @@ import Modal from "./Modal.jsx";
 import styles from "./PostsList.module.css";
 
 function PostsList({ isPosting, stopPosting }) {
+  //  fetch("http://localhost:8080/posts")
+  //    .then((response) => response.json())
+  //    .then((data) => {
+  //      setPosts(data.posts);
+  //    });
   const [posts, setPosts] = useState([]);
+  const [isFecthing, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPosts(data.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     // setPosts([postData, ...posts]);
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }); // POST request to add a new post")
     setPosts((prevPosts) => {
       return [...prevPosts, postData];
     });
@@ -22,14 +46,17 @@ function PostsList({ isPosting, stopPosting }) {
           <NewPost onCancel={stopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length === 0 && <p className={styles.noposts}>No posts found. Start adding some!</p>}
-      {posts.length > 0 && (
+      {!isFecthing && posts.length === 0 && (
+        <p className={styles.noposts}>No posts found. Start adding some!</p>
+      )}
+      {!isFecthing && posts.length > 0 && (
         <ul className={styles.posts}>
           {posts.map((post, index) => {
             return <Post key={index} author={post.author} body={post.body} />;
           })}
         </ul>
       )}
+      {isFecthing && <p className={styles.noposts}>Loading Posts...</p>}
     </>
   );
 }
